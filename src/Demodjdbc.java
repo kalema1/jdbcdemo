18d831c18802467620e4958031d52a71f8535d84
kalema1 <kalemar8@gmail.com>
2026-02-08 11:48:12 +0300

    set up a jdbc demo project

---
+++ src/Demodjdbc.java
@@ -0,0 +1,43 @@
+import java.sql.*; /* step 1 - import the package*/
+
+public class Demodjdbc {
+    public static void main(String[] args){
+        /*
+        * Steps to connect to DB
+        * 1. import package
+        * 2. load and register (optional coz its is done behind the scenes since java4)
+        * 3. create a connection
+        * 4. create a statement
+        * 5. execute the statement
+        * 6 process the results
+        * 7. close the connection
+        * */
+
+        String url = "jdbc:postgresql://localhost:5432/demojavadb";
+        String username = "postgres";
+        String password = "kal123";
+        String sqlQuery = "SELECT * FROM students  WHERE \"Sid\" = 1";
+
+        try {
+//        step 2
+            Class.forName("org.postgresql.Driver");
+
+//            step 3 create a connection to DB. e use the Connection interface from the package
+            Connection con = DriverManager.getConnection(url,username,password);
+            System.out.println("connection established");
+
+//            step 4 create a statement
+            Statement st = con.createStatement();
+
+//            step 5 execute a statement
+            ResultSet results = st.executeQuery(sqlQuery);
+            System.out.println(results.next());
+
+            con.close();
+            System.out.println("connection closed");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
